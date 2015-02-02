8c31ebeb215516b15657af23f3c64f9761c32edf
Matthew Reuther <matthew.reuther@gmail.com>
2015-02-02 11:03:14 -0600

    finialized and competled turning app into all jQuery

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-var $input = $('input'),
+var $input = $('input'),             ///added jQery with $
     $ul    =$('ul'),
     url   = 'https://yspuku7qvh9u4cr3.firebaseio.com/.json';
 
-$input.change(getUpdateAndSplit);
-document.addEventListener('DOMContentLoaded', getUpdateAndSplit);
+//document.addEventListener('DOMContentLoaded', init); ////
+//$(init);
+$(document).ready(init); ////shorthand for commented out or it could be $(init);
+
+function init() {
+  $input = $('input'),
+  $ul    = $('ul');
+
+  $input.change(getUpdateAndSplit);
+  getUpdateAndSplit();
+}
 
 function getUpdateAndSplit(){
-  var count = $input.value;
+  var count = $input.val();  ////made input.val equal the jQery formula
 
   $ul.empty();
-  getJSON(url, function(res){
+  $.get(url, function(res){
     var chunkedStudents = chunkData(res['c8-students'], count);
     $ul.append(createList(chunkedStudents));
   });
@@ -26,7 +35,7 @@ function chunkData(data, count){
 }
 
 function createList(array) {
-  //var docFragment = document.createDocumentFragment();
+  //var docFragment = document.createDocumentFragment(); //////////commented out is the docFragment and is being replaced by jQeury
   var groupList = [];
 
   _.forEach(array, function(team){
@@ -51,15 +60,15 @@ function createList(array) {
   return groupList;
 }
 
-function getJSON(url, cb) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
+//function getJSON(url, cb) {         //////replaced by $.get(url, function(res){
+  //var xhr = new XMLHttpRequest();
+  //xhr.open('GET', url);
 
-  xhr.onload = function () {
-    if (this.status >= 200 && this.status < 400) {
-      cb(JSON.parse(this.response));
-    }
-  };
+  //xhr.onload = function () {
+    //if (this.status >= 200 && this.status < 400) {
+      //cb(JSON.parse(this.response));
+    //}
+  //};
 
-  xhr.send()
-}
+  //xhr.send()
+//}
