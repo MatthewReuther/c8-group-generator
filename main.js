590f84842efd919f3cf7a0dc2f489bd5af680742
Matthew Reuther <matthew.reuther@gmail.com>
2015-02-02 10:28:21 -0600

    use jQery instead of docfragments

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ document.addEventListener('DOMContentLoaded', getUpdateAndSplit);
 
 function getUpdateAndSplit(){
   var count = $input.value;
-  
+
   $ul.empty();
   getJSON(url, function(res){
     var chunkedStudents = chunkData(res['c8-students'], count);
-    $ul.appendChild(createList(chunkedStudents));
+    $ul.append(createList(chunkedStudents));
   });
 };
 
@@ -26,33 +26,40 @@ function chunkData(data, count){
 }
 
 function createList(array) {
-  var docFragment = document.createDocumentFragment();
+  //var docFragment = document.createDocumentFragment();
+  var groupList = [];
 
   _.forEach(array, function(team){
-    var ol = document.createElement('ol');
-    
+   // var ol = document.createElement('ol');
+    var $ol =  $('<ol></ol>');
+
     _.forEach(team, function(teamMember){
-      var li = document.createElement('li');
-      var text = document.createTextNode(teamMember);
-      li.appendChild(text);
-      ol.appendChild(li);
+     // var li = document.createElement('li');
+      var $li =  $('<li >' +  teamMember + '</li>');
+     // var text = document.createTextNode(teamMember);
+     // li.appendChild(text);
+     //  ol.appendChild(li);
+      $ol.append($li);
     })
-    
-    docFragment.appendChild(ol);
-  })
-  
-  return docFragment;
+
+   //docFragment.appendChild(ol);
+   groupList.push($ol);
+
+  });
+
+  //return docFragment;
+  return groupList;
 }
 
 function getJSON(url, cb) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url);
-  
+
   xhr.onload = function () {
     if (this.status >= 200 && this.status < 400) {
       cb(JSON.parse(this.response));
     }
   };
-  
+
   xhr.send()
 }
